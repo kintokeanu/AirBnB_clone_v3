d9b77a18ed45fa110fff339e316b3c0cab0cf1c7
kintokeanu <brianislevu@gmail.com>
2023-10-31 01:51:01 +0300

    fixing api view module

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
-from flask import Flask, jsonify, Blueprint
+from flask import jsonify
 from models import storage
-
 
 
 @app_views.route('/status')
